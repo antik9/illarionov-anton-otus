49dbfd5aad21749568ead289bca48cfd8daa349a
antik9 <illarionovanton@gmail.com>
2019-03-31 13:42:35 +0300

    Fix grammar

--- javascript-1/sum.js
+++ javascript-1/sum.js
@@ -18,7 +18,7 @@ const sum = function(...acc) {
             } else if ( extra.length > 1 ) {
                 return sum(...extra);
             } else if ( typeof(acc[0]) != typeof(extra[0])) {
-                console.error("Different types of arguements")
+                console.error("Different types of arguments")
                 return undefined;
             }
             return sum(acc[0] + extra[0]);
@@ -32,6 +32,6 @@ console.assert(sum(4)(4)(3)(10)(1)() === 22, "not equal to 22")
 console.assert(sum(.4)(.4)(.2)(1)() === 2.0, "not equal to 2.0")
 console.assert(sum("a")("b")("c")() == "abc", "not equal to abc")
 console.assert(sum(undefined) === undefined, "is not undefined")
-console.assert(sum(1, 2) === undefined, "two or mote arguments not allowed")
+console.assert(sum(1, 2) === undefined, "two or more arguments are not allowed")
 console.assert(sum(1)("a") === undefined, "different types are not allowed")
-console.assert(sum({}) === undefined, "bad argument allowed")
+console.assert(sum({}) === undefined, "bad argument is allowed")
